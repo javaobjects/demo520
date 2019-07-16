86f8ca1ce0d6a3c0ae0c9f1cba0d959c29c2f772
yanbo0039 <yanbo0039@yeah.net>
2019-07-16 08:09:10 +0800

    删除图片一

---
+++ src/文件类/TestFile.java
@@ -0,0 +1,14 @@
+package 文件类;
+
+import java.io.File;
+
+public class TestFile {
+
+	public static void main(String[] args) {
+		//1. 如何创建File对象
+		
+		File a = new File("");
+		//2. File类的方法有哪些？
+	}
+
+}
